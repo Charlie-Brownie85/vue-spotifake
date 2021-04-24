54b85d08db38c6e4e92e08d827bec2cc7d3163e3
Charlie-Brownie85 <carlosorbea85@gmail.com>
2021-04-24 21:17:03 +0200

    chore: setting up SCSS and some variables

--- spotify-search-app/vue.config.js
+++ spotify-search-app/vue.config.js
@@ -6,4 +6,15 @@ module.exports = {
       return args;
     });
   },
+  css: {
+    sourceMap: true,
+    requireModuleExtension: false,
+    loaderOptions: {
+      scss: {
+        sourceMap: true,
+        additionalData: `@import "~@/assets/styles/scss/_colors.scss";
+        @import "~@/assets/styles/scss/_variables.scss";`
+      },
+    },
+  },
 };
